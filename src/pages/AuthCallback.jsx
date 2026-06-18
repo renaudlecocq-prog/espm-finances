017f9149e27b6ae1cf74d6e9de71fee39667b6bb
renaudlecocq-prog <renaud.lecocq@gmail.com>
2026-06-18 13:28:07 +0200

    fix: transmettre origin au callback Smartschool

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -33,7 +33,7 @@ export default function AuthCallback() {
       const res = await fetch('/.netlify/functions/smartschool-callback', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ code }),
+        body: JSON.stringify({ code, origin: window.location.origin }),
       })
 
       const data = await res.json()
