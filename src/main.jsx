850b5a4affa81e93813d9808f1e34584583456a0
Renaud Lecocq <renaud.lecocq@gmail.com>
2026-06-26 19:44:42 +0000

    fix: ajouter MantineProvider à la racine (requis par @mantine/core v9 pour BlockNote)

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
 import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <MantineProvider>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </MantineProvider>
   </React.StrictMode>,
 )
